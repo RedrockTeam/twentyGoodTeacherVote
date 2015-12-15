4f6f7e49015b0aaf9fd6ee88992581b3f67f0aa1
Lich2013 <longzy2013@gmail.com>
2015-12-15 20:39:32 +0800

    test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,10 +13,10 @@
 
 
 Route::group(['prefix' => ''], function() {
-    Route::get('/', ['https', 'as' => 'index', 'uses' => 'IndexController@index']); //首页
-    Route::get('norm', ['https', 'as' => 'norm', 'uses' => 'IndexController@norm']); //提名页面
-    Route::get('detail', ['https', 'as' => 'detail', 'uses' => 'IndexController@detail']); //候选人详情
-    Route::get('votePage', ['https', 'as' => 'vote', 'uses' => 'IndexController@vote']); //投票
+    Route::get('/', ['https', ['as' => 'index', 'uses' => 'IndexController@index']]); //首页
+    Route::get('norm', ['as' => 'norm', 'uses' => 'IndexController@norm']); //提名页面
+    Route::get('detail', ['as' => 'detail', 'uses' => 'IndexController@detail']); //候选人详情
+    Route::get('votePage', ['as' => 'vote', 'uses' => 'IndexController@vote']); //投票
     Route::get('talk', ['as' => 'talk', 'uses' => 'IndexController@talk']); //投票
     Route::get('rank', ['as' => 'rank', 'uses' => 'IndexController@rank']); //list
     Route::get('logout', ['as' => 'logout', 'uses' => 'IndexController@logout']); //登出
