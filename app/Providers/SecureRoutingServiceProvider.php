c4256617546f9c6bd04f7af401820495149b82ae
Lich2013 <longzy2013@gmail.com>
2015-12-25 16:47:44 +0800

    test laravel5 force https

---
+++ app/Providers/SecureRoutingServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lich
+ * Date: 15/12/25
+ * Time: 16:44
+ */
+use Illuminate\Routing\UrlGenerator;
+use Illuminate\Routing\RoutingServiceProvider;
+
+class SecureRoutingServiceProvider extends RoutingServiceProvider {
+    public function boot() {
+        App::bind('url', function () {
+            $generator = new UrlGenerator(
+                App::make('router')->getRoutes(),
+                App::make('request')
+
+            );
+
+            $generator->forceSchema('https');
+
+            return $generator;
+        });
+
+        parent::boot();
+    }
+}
